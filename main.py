638bb3e4b5f2c24206627a4b975797b5038a26c8
SensDeLaMort <mr.r.imanov@gmail.com>
2020-03-18 22:55:57 +0400

    Added hello world

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import json
 app = Flask(__name__)
 
 
+@app.route('/')
+def hello():
+    return 'hello world'
+
+
 @app.route('/', method=["POST"])
 def processing():
     data = json.loads(request.data)
